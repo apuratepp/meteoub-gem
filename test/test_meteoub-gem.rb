76dd91c2882d8280d710b24b577d71712bc89fa2
Josep <apuratepp@gmail.com>
2013-05-20 17:12:25 +0200

    add tests

--- test/test_meteoub-gem.rb
+++ test/test_meteoub-gem.rb
@@ -1,7 +1,18 @@
 require 'helper'
 
 class TestMeteoubGem < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  # should "probably rename this file and start testing for real" do
+  #   flunk "hey buddy, you should probably rename this file and start testing for real"
+  # end
+
+  context "Gettings temperature" do
+  	should "get float temperature" do
+  	  dades = MeteoUB::Data.new
+  	  assert_operator(dades.temperature, :>, -10.0)
+	end
+	should "" do
+	  dades = MeteoUB::Data.new
+  	  assert_operator(dades.temperature, :<, 50.0)
+	end
   end
 end
